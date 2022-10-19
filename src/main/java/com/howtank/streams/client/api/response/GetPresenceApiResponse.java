dabb956b60692befc176a1d7418caae1985d482f
vietnguyen <taodo2291@gmail.com>
2022-10-19 23:21:36 +0700

    Fix connection_date deserialize fail due to mismatch format

--- src/main/java/com/howtank/streams/client/api/response/GetPresenceApiResponse.java
+++ src/main/java/com/howtank/streams/client/api/response/GetPresenceApiResponse.java
@@ -3,11 +3,19 @@ package com.howtank.streams.client.api.response;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import com.howtank.streams.model.Presence;
 import com.howtank.streams.model.User;
 import com.howtank.streams.model.types.ConnectionStatusType;
 import lombok.Data;
 
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Set;
@@ -30,6 +38,7 @@ public class GetPresenceApiResponse extends ApiResponse {
 @Data
 class PresenceDto {
     @JsonProperty("connection_date")
+    @JsonDeserialize(using = ConnectionDateDeserializer.class)
     private Date connectionDate;
 
     @JsonProperty("connection_status")
@@ -78,5 +87,26 @@ class PresenceDto {
                         .type(userType)
                         .build())
                 .build();
+    }
+}
+
+class ConnectionDateDeserializer extends StdDeserializer<Date> {
+    public ConnectionDateDeserializer() {
+        this(null);
+    }
+
+    private ConnectionDateDeserializer(Class<?> vc) {
+        super(vc);
+    }
+
+    @Override
+    public Date deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
+        SimpleDateFormat formatter = new SimpleDateFormat("MMM dd, yyyy HH:mm:ss aa");
+        String date = jsonParser.getText();
+        try {
+            return formatter.parse(date);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
